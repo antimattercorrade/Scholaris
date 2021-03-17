91cb93ca8c0a2c31c5512ae0b06aa0e8350ac6b0
adityatripathiiit <adityatripathiball@gmail.com>
2021-03-18 00:34:52 +0530

    update

--- prof/prof/spiders/prof_list.py
+++ prof/prof/spiders/prof_list.py
@@ -6,6 +6,14 @@ import csv
 import re
 import datetime
 
+from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
+from pysummarization.tokenizabledoc.simple_tokenizer import SimpleTokenizer
+from pysummarization.web_scraping import WebScraping
+from pysummarization.abstractabledoc.std_abstractor import StdAbstractor
+from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
+
+
+
 
 filename = "profs.json"  # To save store data
 logging.getLogger('scrapy').propagate = False
@@ -15,7 +23,26 @@ prof_data_list = []
 
 class IntroSpider(scrapy.Spider):
     name = "prof_spider"     # Name of the scraper
-    
+    def summarize(self, url):
+        web_scrape = WebScraping()
+        document = web_scrape.scrape(url)
+        auto_abstractor = AutoAbstractor()
+        auto_abstractor.tokenizable_doc = SimpleTokenizer()
+        auto_abstractor.delimiter_list = [".", "\n"]
+        abstractable_doc = TopNRankAbstractor()
+        result_dict = auto_abstractor.summarize(document, abstractable_doc)
+        
+        # Output 3 summarized sentences.
+        limit = 7
+        i = 1
+        res = ""
+        for sentence in result_dict["summarize_result"]:
+            res+=sentence
+            if i >= limit:
+                break
+            i += 1
+        return(res)
+
     def start_requests(self):
         # getting the complete list of professors from cs ranking, with their homepage urls and google scholar id's
         urls = ['http://csrankings.org/#/index?all&world.html']   
@@ -28,22 +55,27 @@ class IntroSpider(scrapy.Spider):
             for line in csvFile:
                 if(line[-1] == 'scholarid'):
                     continue
-                home_page_url = line[2] 
+                global home_page_url 
                 global scholarID  
                 global Univ_Name 
                 global Prof_Name 
+                global summarized_home_page
+                home_page_url = line[2]
                 scholarID = line[-1]
                 Prof_Name = line[0]
                 Univ_Name = line[1]
+                try:
+                    summarized_home_page = self.summarize(home_page_url)
+                except Exception as ex:
+                    summarized_home_page = None
+                print(summarized_home_page)
                 google_scholar_url = f"https://scholar.google.co.in/citations?user={scholarID}&hl=en&view_op=list_works&sortby=pubdate&pagesize=100"
                 yield scrapy.Request(url = google_scholar_url, callback = self.parse_google_scholar)
-                # print(data)
-                # yield scrapy.Request(url = home_page_url, callback = self.parse_homepage)
-                # prof_data_list.append({"Name": line[0], "University_name": line[1], "Research_Interests": [], "Publications": [], "Scholar_ID": line[-1]})
-        with open("prof.json", 'w+') as f:   # Writing data in the file
-            for data in prof_data_list: 
-                app_json = json.dumps(data)
-                f.write(app_json+"\n")
+                
+        # with open("prof.json", 'w+') as f:   # Writing data in the file
+        #     for data in prof_data_list: 
+        #         app_json = json.dumps(data)
+        #         f.write(app_json+"\n")
     
 
     def parse_google_scholar(self, response):
@@ -65,12 +97,11 @@ class IntroSpider(scrapy.Spider):
             publications.append([name, venue, year]) 
             row_number += 1
 
-        prof_data_list.append({"Name": Prof_Name, "University_name": Univ_Name, "H Index": h_index, "img_src": img_src_url , "Citations": citations, "I10 Index": i10_index,"Research_Interests": prof_interests, "Publications": publications, "Scholar_ID": scholarID})
-        
+        prof_data_list.append({"Name": Prof_Name, "University_name": Univ_Name, "H Index": h_index, "img_src": img_src_url , "Citations": citations, "I10 Index": i10_index,"Research_Interests": prof_interests, "Publications": publications, "Scholar_ID": scholarID, "home_page_url": home_page_url, "home_page_summary": summarized_home_page})
+        with open("prof.json", 'a+') as f:   # Writing data in the file
+            app_json = json.dumps({"Name": Prof_Name, "University_name": Univ_Name, "H Index": h_index, "img_src": img_src_url , "Citations": citations, "I10 Index": i10_index,"Research_Interests": prof_interests, "Publications": publications, "Scholar_ID": scholarID, "home_page_url": home_page_url, "home_page_summary": summarized_home_page})
+            f.write(app_json+"\n")
 
-    
-    def parse_homepage(self,response):
-        print("aagaya")
 
 
     def parse(self, response):
