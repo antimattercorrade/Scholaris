245e64e240234df71f4ac8b8e4f3012d55e9c3cf
adityatripathiiit <adityatripathiball@gmail.com>
2021-03-17 23:27:01 +0530

    update

--- prof/prof/spiders/prof_list.py
+++ prof/prof/spiders/prof_list.py
@@ -5,20 +5,17 @@ import requests
 import csv
 import re
 import datetime
-from scrapy_selenium import SeleniumRequest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
-# from selenium import webdriver
-
-# driver = webdriver.Chrome(executable_path="../../chromedriver")
 
 filename = "profs.json"  # To save store data
 logging.getLogger('scrapy').propagate = False
 
+prof_data_list = []
+
+
 class IntroSpider(scrapy.Spider):
     name = "prof_spider"     # Name of the scraper
-
+    
     def start_requests(self):
         # getting the complete list of professors from cs ranking, with their homepage urls and google scholar id's
         urls = ['http://csrankings.org/#/index?all&world.html']   
@@ -27,104 +24,60 @@ class IntroSpider(scrapy.Spider):
         
         with open('prof.csv', mode ='r',encoding='utf8')as file: 
             csvFile = csv.reader(file)
-            next(csvFile) 
-            prof_data_list = []            
+            next(csvFile)             
             for line in csvFile:
                 if(line[-1] == 'scholarid'):
                     continue
                 home_page_url = line[2] 
-                scholarID = line[-1] 
-                google_scholar_url = f"https://scholar.google.co.in/citations?user=plJC8R0AAAAJ&hl=en&view_op=list_works&sortby=pubdate"
-                # data = yield scrapy.Request(url = google_scholar_url, callback = self.parse_google_scholar)
-                yield SeleniumRequest( 
-                    url =google_scholar_url, 
-                    wait_time = 3, 
-                    screenshot = False, 
-                    callback = self.parse_google_scholar,  
-                    wait_until= EC.element_to_be_clickable((By.ID, 'gsc_bpf_more'))
-                )
-                break
+                global scholarID  
+                global Univ_Name 
+                global Prof_Name 
+                scholarID = line[-1]
+                Prof_Name = line[0]
+                Univ_Name = line[1]
+                google_scholar_url = f"https://scholar.google.co.in/citations?user={scholarID}&hl=en&view_op=list_works&sortby=pubdate&pagesize=100"
+                yield scrapy.Request(url = google_scholar_url, callback = self.parse_google_scholar)
+                # print(data)
                 # yield scrapy.Request(url = home_page_url, callback = self.parse_homepage)
-                prof_data_list.append({"Name": line[0], "University_name": line[1], "Research_Interests": [], "Publications": [], "Scholar_ID": line[-1]})
-
+                # prof_data_list.append({"Name": line[0], "University_name": line[1], "Research_Interests": [], "Publications": [], "Scholar_ID": line[-1]})
+        with open("prof.json", 'w+') as f:   # Writing data in the file
+            for data in prof_data_list: 
+                app_json = json.dumps(data)
+                f.write(app_json+"\n")
     
 
     def parse_google_scholar(self, response):
         prof_interests = response.css('div[id="gsc_prf_int"] > a::text').extract()
+        citations = response.xpath('//div[@id="gsc_rsb_cit"]/table[@id="gsc_rsb_st"]/tbody/tr[1]/td[2]/text()').extract_first()
+        h_index = response.xpath('//div[@id="gsc_rsb_cit"]/table[@id="gsc_rsb_st"]/tbody/tr[2]/td[2]/text()').extract_first()
+        i10_index = response.xpath('//div[@id="gsc_rsb_cit"]/table[@id="gsc_rsb_st"]/tbody/tr[3]/td[2]/text()').extract_first()
+        img_src_url = response.xpath('//img[@id="gsc_prf_pup-img"]/@src').extract_first()
+ 
         row_number = 1
+        publications = []
         while(True):
             row_sel = response.xpath(f'//div[@id="gsc_a_tw"]/table/tbody/tr[{row_number}]')
             name = row_sel.xpath(".//td/a/text()").extract_first()
             venue = row_sel.xpath(".//td/div[2]/text()").extract_first()
             year = row_sel.xpath(".//td/span/text()").extract_first()
-            if(int(year)< datetime.datetime.now().year -5 ):
-                flag = False
+            if(name == None or year == None or int(year)< datetime.datetime.now().year -5 ):
                 break
-            # print([name,venue,year])
-            if response.xpath("//button[@class='gs_btnPR gs_in_ib gs_btn_half gs_btn_lsb gs_btn_srt gsc_pgn_pnx']/@onclick").extract_first() != "" and row_number%20 == 0:
-                print("more entries")
-                # response.xpath("//button[@id='gsc_bpf_more']/@onclick").extract_first().replace("\\x3d","=").replace("\\x26","?")
+            publications.append([name, venue, year]) 
             row_number += 1
 
-
-                
-			# link = prof_sel.xpath(".//table/tbody").extract_first()
-			# url = response.urljoin(link)
-			# yield scrapy.Request(url,callback=self.parse_url_to_crawl)
-		
-		
-        # prof_publications_name = response.xpath('//div[@id="gsc_a_tw"]/table/tbody/tr/td:nth-child(1)/a/text()').get()
-        # prof_publications_year = response.xpath('//div[@id="gsc_a_tw"]/table/tbody/tr/td:nth-child(3)/span/text()').get()
+        prof_data_list.append({"Name": Prof_Name, "University_name": Univ_Name, "H Index": h_index, "img_src": img_src_url , "Citations": citations, "I10 Index": i10_index,"Research_Interests": prof_interests, "Publications": publications, "Scholar_ID": scholarID})
         
-        # prof_publications_name = response.css('div[id="gsc_a_tw"] > table > tbody > tr > td:nth-child(1) > a::text').extract()
-        # prof_publications_venue = response.css('div[id="gsc_a_tw"] > table > tbody > tr > td:nth-child(1) > div:nth-child(3)::text').extract()
-        # prof_publications_year = response.css('div[id="gsc_a_tw"] > table > tbody > tr > td:nth-child(3) > span::text').extract()
-        
-        # n = len(prof_publications_year) 
-        # for i in range(n):
-        #     if(int(prof_publications_year[i]) >=  datetime.datetime.now().year -5):
-        #         print([prof_publications_name[i],prof_publications_year[i]])
 
-
-        # for publication in prof_publications_name:
-        #     print(publication)
-        #     break
-        # print(prof_publications)
-        # return prof_interests
-        # print(prof_interests)
     
     def parse_homepage(self,response):
         print("aagaya")
-        # if re.search("Neuroscience",response.text):
-        #     print("Neuroscience")
-        
-        # print(response.text)
+
 
     def parse(self, response):
-        list_data=[]
         prof_csv_url = response.css('p.text-muted > a:nth-child(2)::attr(href)').extract()[-1] # accessing the titles
         prof_csv_url = prof_csv_url.replace("blob", "raw")
         # print(prof_csv_url)
         r = requests.get(prof_csv_url)
         open('./prof.csv', 'wb').write(r.content)
-
-        
         
 
-
-        # i=0
-        # for book_title in book_list:
-        #     data={
-        #         'book_title' : book_title,
-        #         'price' : price_list[i],
-        #         'image-url' : image_link[i],
-        #         'url' : link_list[i]
-        #     }
-        #     i+=1
-        #     list_data.append(data)
-            
-        # with open(filename, 'a+') as f:   # Writing data in the file
-        #     for data in list_data : 
-        #         app_json = json.dumps(data)
-        #         f.write(app_json+"\n")
-# IntroSpider.fetch_data()
